0c5c07311ef0bd55eba39b035c1d9b0f44a4ff13
Rodrigo Lara <ereodeereigeo@gmail.com>
2020-01-14 13:03:19 -0300

    cambiar frecuencia de muestreo a 2 minutos

--- camion-05.ino
+++ camion-05.ino
@@ -427,7 +427,7 @@ sensors.begin();
 }
 
 void loop() {
-  delay(59000); //delay de 1 segundo antes de ejecutar el resto del código, al final del código es un delay de 59 seg para en resultado 
+  delay(119000); //delay de 1 segundo antes de ejecutar el resto del código, al final del código es un delay de 59 seg para en resultado 
   //crear un loop que lea los datos cada 1 minuto y los envíe por internet a la plataforma
   // put your main code here, to run repeatedly:
 //código para testear los comandos AT desde la consola serial
