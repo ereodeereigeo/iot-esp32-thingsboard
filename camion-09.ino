d78cc8a9be20d3156fd731a1ddab69d220f4b63a
Rodrigo Lara <ereodeereigeo@gmail.com>
2020-01-24 09:57:41 -0300

    Update camion-09.ino

--- camion-09.ino
+++ camion-09.ino
@@ -515,6 +515,10 @@ appendFile(SD, "/data.txt", result);
     if (!modem.waitForNetwork()) {
         SerialMon.println(" fail");
         delay(1000);
+        pinMode(4,OUTPUT);
+        digitalWrite(4, HIGH);   // set the RTS off
+        delay(1000);
+        digitalWrite(4, LOW);   // set the RTS on
         return;
     }
     SerialMon.println(" OK");
